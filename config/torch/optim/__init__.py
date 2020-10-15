5d4455a75077af1788d7660f773d0d96aecfea0d
Rosario Scalise <rosario@cs.washington.edu>
2020-10-15 11:21:28 -0700

    Add back all optimizers. Enable imports mirroring torch.

---
+++ config/torch/optim/__init__.py
@@ -0,0 +1,25 @@
+
+from .adadelta import AdadeltaConf
+from .adagrad import AdagradConf
+from .adam import AdamConf
+from .adamw import AdamWConf
+from .sparse_adam import SparseAdamConf
+from .adamax import AdamaxConf
+from .asgd import ASGDConf
+from .sgd import SGDConf
+from .rprop import RpropConf
+from .rmsprop import RMSpropConf
+from .lbfgs import LBFGSConf
+#from . import lr_scheduler
+
+del adadelta
+del adagrad
+del adam
+del adamw
+del sparse_adam
+del adamax
+del asgd
+del sgd
+del rprop
+del rmsprop
+del lbfgs
