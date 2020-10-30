5c78f34f447817d581c7885bb313e879cafea17b
Rosario Scalise <rosario@cs.washington.edu>
2020-10-30 12:47:38 -0700

    configure dataloaders

--- examples/mnist_01.py
+++ examples/mnist_01.py
@@ -17,6 +17,8 @@ from typing import Any
 # hydra-torch structured config imports
 from config.torch.optim import AdadeltaConf
 from config.torch.optim.lr_scheduler import StepLRConf
+from config.torch.utils.data import DataLoaderConf
+
 
 @dataclass
 class MNISTNetConf:
@@ -30,8 +32,6 @@ class MNISTNetConf:
 
 @dataclass
 class MNISTConf:
-    batch_size: int = 64
-    test_batch_size: int = 1000
     epochs: int = 14
     no_cuda: bool = False
     dry_run: bool = False
@@ -39,13 +39,15 @@ class MNISTConf:
     log_interval: int = 10
     save_model: bool = False
     checkpoint_name: str = "unnamed.pt"
+    data_train: DataLoaderConf = DataLoaderConf(
+        batch_size=64, shuffle=True, num_workers=1, pin_memory=False
+    )
+    data_test: DataLoaderConf = DataLoaderConf(
+        batch_size=1000, shuffle=False, num_workers=1
+    )
     model: MNISTNetConf = MNISTNetConf()
     optim: Any = AdadeltaConf()
-    scheduler: Any = StepLRConf(
-        step_size=1
-    )  # we pass a default for step_size since it is required, but missing a default in PyTorch (and consequently in hydra-torch)
-
-
+    scheduler: Any = StepLRConf(step_size=1)
 
 
 cs = ConfigStore.instance()
@@ -55,6 +57,7 @@ cs.store(name="mnistconf", node=MNISTConf)
 
 
 class Net(nn.Module):
+    # DIFF: new model definition
     def __init__(self, input_shape, output_shape, cfg):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, cfg.model.conv1_out_channels, 3, 1)
@@ -71,12 +74,13 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(linear_in_shape, cfg.model.fc_hidden_features)
         self.fc2 = nn.Linear(cfg.model.fc_hidden_features, output_shape[1])
 
+    # DIFF: new method
     def _compute_conv_out_shape(self, input_shape):
         dummy_input = torch.zeros(input_shape).unsqueeze(0)
         with torch.no_grad():
             x = self.conv1(dummy_input)
             x = self.conv2(x)
-            dummy_output = self.maxpool1(x) 
+            dummy_output = self.maxpool1(x)
         return dummy_output.shape
 
     def forward(self, x):
@@ -84,7 +88,7 @@ class Net(nn.Module):
         x = F.relu(x)
         x = self.conv2(x)
         x = F.relu(x)
-        x = self.maxpool1(x) # DIFF
+        x = self.maxpool1(x)  # DIFF
         x = self.dropout1(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
@@ -149,39 +153,41 @@ def test(model, device, test_loader):
 @hydra.main(config_name="mnistconf")
 def main(cfg):  # DIFF
     print(cfg.pretty())
-    use_cuda = not cfg.no_cuda and torch.cuda.is_available()  # DIFF
-    torch.manual_seed(cfg.seed)  # DIFF
+    use_cuda = not cfg.no_cuda and torch.cuda.is_available()
+    torch.manual_seed(cfg.seed)
     device = torch.device("cuda" if use_cuda else "cpu")
 
-    train_kwargs = {"batch_size": cfg.batch_size}  # DIFF
-    test_kwargs = {"batch_size": cfg.test_batch_size}  # DIFF
-    if use_cuda:
-        cuda_kwargs = {"num_workers": 1, "pin_memory": True, "shuffle": True}
-        train_kwargs.update(cuda_kwargs)
-        test_kwargs.update(cuda_kwargs)
+    # train_kwargs = {"batch_size": cfg.batch_size}
+    # test_kwargs = {"batch_size": cfg.test_batch_size}
+    # if use_cuda:
+    #    cuda_kwargs = {"num_workers": 1, "pin_memory": True, "shuffle": True}
+    #    train_kwargs.update(cuda_kwargs)
+    #    test_kwargs.update(cuda_kwargs)
 
     transform = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
     )
-    dataset1 = datasets.MNIST("../data", train=True, download=True, transform=transform)
-    dataset2 = datasets.MNIST("../data", train=False, transform=transform)
-    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
-    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+    dataset_train = datasets.MNIST(
+        "../data", train=True, download=True, transform=transform
+    )
+    dataset_test = datasets.MNIST("../data", train=False, transform=transform)
+    train_loader = instantiate(cfg.data_train, dataset=dataset_train)  # DIFF
+    test_loader = instantiate(cfg.data_test, dataset=dataset_test)  # DIFF
 
     input_shape = (1, 28, 28)
     output_shape = (1, 10)
-    model = Net(input_shape, output_shape, cfg).to(device)
+    model = Net(input_shape, output_shape, cfg).to(device)  # DIFF
 
     optimizer = instantiate(cfg.optim, params=model.parameters())  # DIFF
     scheduler = instantiate(cfg.scheduler, optimizer=optimizer)  # DIFF
 
-    for epoch in range(1, cfg.epochs + 1):  
-        train(cfg, model, device, train_loader, optimizer, epoch)  
+    for epoch in range(1, cfg.epochs + 1):
+        train(cfg, model, device, train_loader, optimizer, epoch)
         test(model, device, test_loader)
         scheduler.step()
 
-    if cfg.save_model: 
-        torch.save(model.state_dict(), cfg.checkpoint_name) 
+    if cfg.save_model:
+        torch.save(model.state_dict(), cfg.checkpoint_name)
 
 
 if __name__ == "__main__":
