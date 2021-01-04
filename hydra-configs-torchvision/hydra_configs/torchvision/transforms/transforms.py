e32675f73b400f81c2dd9f48dcd1f9c00f4a68ee
romesc <rosario@cs.washington.edu>
2021-01-03 22:25:52 -0800

    regen with finalized configen changes

--- hydra-configs-torchvision/hydra_configs/torchvision/transforms/transforms.py
+++ hydra-configs-torchvision/hydra_configs/torchvision/transforms/transforms.py
@@ -14,14 +14,14 @@ from typing import Any
 @dataclass
 class CenterCropConf:
     _target_: str = "torchvision.transforms.transforms.CenterCrop"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     size: Any = MISSING
 
 
 @dataclass
 class ColorJitterConf:
     _target_: str = "torchvision.transforms.transforms.ColorJitter"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     brightness: Any = 0
     contrast: Any = 0
     saturation: Any = 0
@@ -31,42 +31,42 @@ class ColorJitterConf:
 @dataclass
 class ComposeConf:
     _target_: str = "torchvision.transforms.transforms.Compose"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     transforms: Any = MISSING
 
 
 @dataclass
 class ConvertImageDtypeConf:
     _target_: str = "torchvision.transforms.transforms.ConvertImageDtype"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     dtype: Any = MISSING  # dtype
 
 
 @dataclass
 class FiveCropConf:
     _target_: str = "torchvision.transforms.transforms.FiveCrop"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     size: Any = MISSING
 
 
 @dataclass
 class GrayscaleConf:
     _target_: str = "torchvision.transforms.transforms.Grayscale"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     num_output_channels: Any = 1
 
 
 @dataclass
 class LambdaConf:
     _target_: str = "torchvision.transforms.transforms.Lambda"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     lambd: Any = MISSING
 
 
 @dataclass
 class LinearTransformationConf:
     _target_: str = "torchvision.transforms.transforms.LinearTransformation"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     transformation_matrix: Any = MISSING
     mean_vector: Any = MISSING
 
@@ -74,7 +74,7 @@ class LinearTransformationConf:
 @dataclass
 class NormalizeConf:
     _target_: str = "torchvision.transforms.transforms.Normalize"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     mean: Any = MISSING
     std: Any = MISSING
     inplace: Any = False
@@ -83,7 +83,7 @@ class NormalizeConf:
 @dataclass
 class PadConf:
     _target_: str = "torchvision.transforms.transforms.Pad"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     padding: Any = MISSING
     fill: Any = 0
     padding_mode: Any = "constant"
@@ -92,13 +92,13 @@ class PadConf:
 @dataclass
 class PILToTensorConf:
     _target_: str = "torchvision.transforms.transforms.PILToTensor"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
 
 
 @dataclass
 class RandomAffineConf:
     _target_: str = "torchvision.transforms.transforms.RandomAffine"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     degrees: Any = MISSING
     translate: Any = None
     scale: Any = None
@@ -110,7 +110,7 @@ class RandomAffineConf:
 @dataclass
 class RandomApplyConf:
     _target_: str = "torchvision.transforms.transforms.RandomApply"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     transforms: Any = MISSING
     p: Any = 0.5
 
@@ -118,14 +118,14 @@ class RandomApplyConf:
 @dataclass
 class RandomChoiceConf:
     _target_: str = "torchvision.transforms.transforms.RandomChoice"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     transforms: Any = MISSING
 
 
 @dataclass
 class RandomCropConf:
     _target_: str = "torchvision.transforms.transforms.RandomCrop"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     size: Any = MISSING
     padding: Any = None
     pad_if_needed: Any = False
@@ -136,7 +136,7 @@ class RandomCropConf:
 @dataclass
 class RandomErasingConf:
     _target_: str = "torchvision.transforms.transforms.RandomErasing"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     p: Any = 0.5
     scale: Any = (0.02, 0.33)
     ratio: Any = (0.3, 3.3)
@@ -147,28 +147,28 @@ class RandomErasingConf:
 @dataclass
 class RandomGrayscaleConf:
     _target_: str = "torchvision.transforms.transforms.RandomGrayscale"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     p: Any = 0.1
 
 
 @dataclass
 class RandomHorizontalFlipConf:
     _target_: str = "torchvision.transforms.transforms.RandomHorizontalFlip"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     p: Any = 0.5
 
 
 @dataclass
 class RandomOrderConf:
     _target_: str = "torchvision.transforms.transforms.RandomOrder"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     transforms: Any = MISSING
 
 
 @dataclass
 class RandomPerspectiveConf:
     _target_: str = "torchvision.transforms.transforms.RandomPerspective"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     distortion_scale: Any = 0.5
     p: Any = 0.5
     interpolation: Any = 2
@@ -178,7 +178,7 @@ class RandomPerspectiveConf:
 @dataclass
 class RandomResizedCropConf:
     _target_: str = "torchvision.transforms.transforms.RandomResizedCrop"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     size: Any = MISSING
     scale: Any = (0.08, 1.0)
     ratio: Any = (0.75, 1.3333333333333333)
@@ -188,7 +188,7 @@ class RandomResizedCropConf:
 @dataclass
 class RandomRotationConf:
     _target_: str = "torchvision.transforms.transforms.RandomRotation"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     degrees: Any = MISSING
     resample: Any = False
     expand: Any = False
@@ -199,21 +199,21 @@ class RandomRotationConf:
 @dataclass
 class RandomTransformsConf:
     _target_: str = "torchvision.transforms.transforms.RandomTransforms"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     transforms: Any = MISSING
 
 
 @dataclass
 class RandomVerticalFlipConf:
     _target_: str = "torchvision.transforms.transforms.RandomVerticalFlip"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     p: Any = 0.5
 
 
 @dataclass
 class ResizeConf:
     _target_: str = "torchvision.transforms.transforms.Resize"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     size: Any = MISSING
     interpolation: Any = 2
 
@@ -221,7 +221,7 @@ class ResizeConf:
 @dataclass
 class TenCropConf:
     _target_: str = "torchvision.transforms.transforms.TenCrop"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     size: Any = MISSING
     vertical_flip: Any = False
 
@@ -229,11 +229,11 @@ class TenCropConf:
 @dataclass
 class ToPILImageConf:
     _target_: str = "torchvision.transforms.transforms.ToPILImage"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
     mode: Any = None
 
 
 @dataclass
 class ToTensorConf:
     _target_: str = "torchvision.transforms.transforms.ToTensor"
-    _convert_: str = "all"
+    _convert_: str = "ALL"
