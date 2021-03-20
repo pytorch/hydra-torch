d485459ecedf219dbf426e85710f3fad10f74271
rosario <rosario@cs.washington.edu>
2021-03-20 14:24:16 -0700

    reorganize requirements

--- hydra-configs-torch/setup.py
+++ hydra-configs-torch/setup.py
@@ -1,6 +1,10 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from setuptools import find_namespace_packages, setup
 
+requirements = [
+    "omegaconf",
+]
+
 setup(
     name="hydra-configs-torch",
     version="1.6.1",
@@ -9,7 +13,5 @@ setup(
     author_email=["omry@fb.com", "rosario@cs.uw.edu"],
     url="http://github.com/pytorch/hydra-torch",
     include_package_data=True,
-    install_requires=[
-        "omegaconf",
-    ],
+    install_requires=requirements,
 )
